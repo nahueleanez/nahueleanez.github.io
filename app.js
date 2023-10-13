3abb97259b9dc990e7d7b71fef8bfadf9339c58b
nahueleanez <89610723+nahueleanez@users.noreply.github.com>
2023-10-13 17:38:32 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const desplazarArriba = document.querySelector("#scroll-up");
+
+desplazarArriba.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+  });
+});
+
+const burger = document.querySelector("#burguer-menu"); // Cambié "burguer" a "burger" aquí
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+  ul.classList.toggle("show");
+});
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
